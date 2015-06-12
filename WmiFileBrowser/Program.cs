6b38a24446ff906c022d361424c4affb19d41346
feratur <feraturdev@gmail.com>
2015-06-13 02:37:20 +0300

    some auxiliary WMI methods

---
+++ WmiFileBrowser/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WmiFileBrowser
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                var g = WmiUtils.GetConnectedScope("localhost", @"root\cimv2", null, null);
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
---
+++ WmiFileBrowser/WmiUtils.cs
@@ -0,0 +1,58 @@
+﻿using System.Management;
+using System.Security;
+
+namespace WmiFileBrowser
+{
+    static class WmiUtils
+    {
+        public static ConnectionOptions CreateConnectionOptions(string login, SecureString password)
+        {
+            return new ConnectionOptions
+            {
+                Authentication = AuthenticationLevel.PacketPrivacy,
+                EnablePrivileges = true,
+                Impersonation = ImpersonationLevel.Impersonate,
+                Locale = "MS_409",
+                SecurePassword = password != null ? password.Copy() : null,
+                Username = login
+            };
+        }
+
+        public static ManagementScope CreateScope(string address, string wmiNamespace, ConnectionOptions options)
+        {
+            return
+                new ManagementScope(
+                    new ManagementPath
+                    {
+                        NamespacePath = wmiNamespace,
+                        Server = address
+                    }, options);
+        }
+
+        public static ManagementScope GetConnectedScope(string address, string wmiNamespace, string login,
+            SecureString password)
+        {
+            var scope = CreateScope(address, wmiNamespace, CreateConnectionOptions(login, password));
+            try
+            {
+                scope.Connect();
+            }
+            catch (ManagementException ex)
+            {
+                switch (ex.ErrorCode)
+                {
+                    case ManagementStatus.LocalCredentials:
+                        scope =
+                            new ManagementScope(string.IsNullOrEmpty(address)
+                                ? wmiNamespace
+                                : @"\\" + address + @"\" + wmiNamespace);
+                        scope.Connect();
+                        break;
+                    default:
+                        throw;
+                }
+            }
+            return scope;
+        }
+    }
+}
